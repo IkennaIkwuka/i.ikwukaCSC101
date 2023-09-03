73146ac9166c8cfa5216ff4ed13bea09eb591ce7
Ikenna Ikwuka <ikennanickikwuka2@gmail.com>
2023-09-03 23:43:50 +0100

    03/09/2023

--- week-3/practice_1/target/debug/deps/practice_1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\ciel\Documents\i.ikwukaCSC101\week-3\practice_1\target\debug\deps\practice_1.exe: src\main.rs
-
-C:\Users\ciel\Documents\i.ikwukaCSC101\week-3\practice_1\target\debug\deps\practice_1.d: src\main.rs
-
-src\main.rs:
